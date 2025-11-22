bfc0ed1f81c94830131ebb8c5c1b89443cc0ad2c
Davi de Oliveira <olidavi031130@gmail.com>
2025-11-22 14:04:46 -0300

    feat: customized permission to add members to groups

--- src/apps/groups/permissions.py
+++ src/apps/groups/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+from .models import Group, GroupMember
 
 
 class IsGroupCreatorOrReadOnly(BasePermission):
@@ -13,4 +14,32 @@ class IsGroupCreatorOrReadOnly(BasePermission):
         
         # Apenas o criador pode editar ou deletar
         return obj.creator == request.user
-    
+
+
+class IsGroupOwnerToAddMembers(BasePermission):
+    """
+    Dono pode adicionar/remover membros, membros podem apenas ver.
+    """
+    def has_permission(self, request, view):
+        group_pk = view.kwargs.get("group_pk")
+
+        try:
+            group = Group.objects.get(id=group_pk)
+            is_member = GroupMember.objects.filter(group=group, user=request.user).exists()
+
+            # GET pra qualquer membro
+            if request.method in SAFE_METHODS:
+                return is_member or group.creator == request.user
+            
+            # POST/DELETE apenas para criador
+            return group.creator == request.user
+        except Group.DoesNotExist:
+            return False
+        
+    def has_object_permission(self, request, view, obj):
+        # GET pra qualquer membro
+        if request.method in SAFE_METHODS:
+            return GroupMember.objects.filter(group=obj.group, user=request.user).exists()
+        
+        # DELETE apenas para o criador
+        return obj.group.creator == request.user
