2819b5eaeb505694f38465a146be5d6e3fc857a5
Davi de Oliveira <olidavi031130@gmail.com>
2025-11-22 12:25:48 -0300

    feat: models for groups

--- src/apps/groups/models.py
+++ src/apps/groups/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+from django.db.models import (
+    Model,
+    CharField,
+    TextField,
+    ForeignKey,
+    DateTimeField,
+    PROTECT,
+    CASCADE,
+)
+from apps.users.models import User
 
-# Create your models here.
+
+class Group(Model):
+    creator = ForeignKey(User, on_delete=PROTECT, related_name="created_groups")
+    name = CharField(max_length=60, null=False, blank=False)
+    description = TextField()
+    color = CharField(null=False, blank=False)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "groups"
+
+
+class GroupMember(Model):
+    group = ForeignKey(Group, on_delete=CASCADE, related_name="group_members")
+    user = ForeignKey(User, on_delete=CASCADE, related_name="group_memberships")
+    joined_at = DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "group_members"
