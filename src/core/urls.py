394831f452a2f89df98955141cd2f9d87a7e074d
Davi de Oliveira <olidavi031130@gmail.com>
2025-11-23 11:53:50 -0300

    feat: customized permissions for events viewset

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
+from core import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("docs/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
-    path("api/v1/auth/", include("apps.authentication.urls")),
-    path("api/v1/users/", include("apps.users.urls")),
-    path("api/v1/groups/", include("apps.groups.urls")),
+    path(f"api/{settings.API_MAJOR}/auth/", include("apps.authentication.urls")),
+    path(f"api/{settings.API_MAJOR}/users/", include("apps.users.urls")),
+    path(f"api/{settings.API_MAJOR}/groups/", include("apps.groups.urls")),
+    path(f"api/{settings.API_MAJOR}/events/", include("apps.events.urls")),
 ]
--- src/utils/permissions.py
+++ src/utils/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import DjangoModelPermissions
+from apps.users.enums import Role
 
 
 class IsAdmin(DjangoModelPermissions):
@@ -6,7 +7,7 @@ class IsAdmin(DjangoModelPermissions):
     Permissão customizada que garante acesso se o usuário for admin.
     """
     def has_permission(self, request, view):
-        if request.user.role == "admin" and request.user.is_superuser:
+        if request.user.role == Role.ADMIN and request.user.is_staff:
             return True
         
 
