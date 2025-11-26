5aa7783a95fc402ed71e4d9bca93850edcbc03cd
Davi de Oliveira <olidavi031130@gmail.com>
2025-11-26 13:08:32 -0300

    chore: customizing admin page for users

--- src/apps/users/admin.py
+++ src/apps/users/admin.py
@@ -1,4 +1,26 @@
+# apps/users/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'created_at')}),
+        ('Additional info', {'fields': ('role', 'preferred_calendar_view')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2', 'role'),
+        }),
+    )
+    
+    list_display = ('email', 'name', 'role', 'is_staff', 'is_active')
+    search_fields = ('email', 'name')
+    ordering = ('email',)
+
+admin.site.register(User, CustomUserAdmin)
--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -33,6 +33,7 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('role', Role.ADMIN)
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
