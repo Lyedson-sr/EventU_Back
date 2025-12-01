9d18565b6ae642e7f39b62442516ba3fb51460eb
Davi de Oliveira <olidavi031130@gmail.com>
2025-12-01 15:41:43 -0300

    fix: changed router register order in events urls file

--- src/apps/events/urls.py
+++ src/apps/events/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import EventsViewSet, EventOccurrencesViewSet
 
 router = DefaultRouter()
-router.register(r"", EventsViewSet, basename="events")
 router.register(r"occurrences", EventOccurrencesViewSet, basename="occurrences")
+router.register(r"", EventsViewSet, basename="events")
 
 urlpatterns = [
     path("", include(router.urls)),
